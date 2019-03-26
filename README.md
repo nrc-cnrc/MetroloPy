26fd0da99efa4eac95c60483f4ddb9bb51f36220
hvparks <48930611+hvparks@users.noreply.github.com>
2019-03-26 17:29:21 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+
+# MetroloPy
+
+tools for dealing with physical quantities:  uncertainty propagation and unit conversion
+
+---
+
+MetroloPy is a pure python package and requires Python 3 and the SciPy stack (NumPy, SciPy, Pandas, and IPython).  It looks best in a Jupyter Notebook.
+
+Install MetroloPy with pip:
+
+```
+$ pip install metrolopy
+```
+
+Physical quantities can then be represented in Python as `gummy` objects:
+
+```
+>>> import metrolopy as uc
+>>> a = uc.gummy(1.2345,u=0.0234,unit='cm')
+>>> a
+1.234(23) cm
+
+>>> b = uc.gummy(3.034,u=0.174,unit='mm')
+>>> f = uc.gummy(uc.UniformDist(center=0.9345,half_width=0.096),unit='N')
+>>> p = f/(a*b)
+>>> p
+2.50(21) N/cm²
+
+>>> p.unit = 'kPa'
+>>> p.uunit = '%'
+>>> p
+25.0 kPa ± 8.5%
+```
+
+MetroloPy can do much more including Monte-Carlo uncertainty propagation, generating uncertainty budget tables, and curve fitting.  It can also handle expanded uncertainties, degrees of freedom, correlated quantities, and complex valued quantities. See:
+
+* [a tutorial](https://nrc-cnrc.github.io/MetroloPy/_static/tutorial.html) (or  <a href="https://nrc-cnrc.github.io/MetroloPy/_downloads/tutorial.ipynb" download> download the tutorial as Jupyter notebook</a>)
+* [the documentation](https://nrc-cnrc.github.io/MetroloPy/)
+* [the issues page on GitHub](https://github.com/nrc-cnrc/Metrolopy/issues)
---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 10 09:36:41 2017
+
+@author: h parks
+"""
+
+from setuptools import setup
+
+__version__ = None
+with open('metrolopy/version.py') as f:
+    exec(f.read())
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+            
+setup(name = 'metrolopy',
+      version = __version__,
+      description = 'tools for dealing with measured quantities: uncertainty propagation and unit conversion',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author = 'Harold Parks, National Research Council Canada',
+      author_email = 'parksh@nrc.ca',
+      url = 'http://nrc-cnrc.github.io/MetroloPy/',
+      packages = ['metrolopy','metrolopy.tests'],
+      package_data = {'metrolopy':['license.txt']},
+      setup_requires = ['numpy'],
+      install_requires=['scipy','matplotlib','pandas'],
+      extras_require = {'pretty':['IPython']},
+      zip_safe = True,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Framework :: Jupyter",
+        "Framework :: IPython",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Education",
+        "Topic :: Scientific/Engineering :: Physics"
+        ]   
+      )
+
+
+    
+    
