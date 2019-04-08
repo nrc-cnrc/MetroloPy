a3ce7c001d03356d4c741a765cb19fc8e7dc0fe0
hvparks <48930611+hvparks@users.noreply.github.com>
2019-04-08 11:37:20 -0400

    tests

--- metrolopy/tests/test_operations.py
+++ metrolopy/tests/test_operations.py
@@ -65,6 +65,8 @@ def binary_func(f,df,sim=False,exp=None,fionly=False,uexp=-6,allowazero=True,
     if dof is not None and not np.isinf(dof):
         assert abs((g.dof - dof)/dof) < 0.01
     
+    assert a.correlation(b) == 0
+    
     if sim and x != 0 and abs(u/x) > 1e-10:
         uc.gummy.simulate([a,b,g])
         
